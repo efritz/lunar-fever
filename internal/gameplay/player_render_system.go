77d191cd99a4de27dc54c740d7b6d6d80c5b10a5
Eric Fritz <eric@eric-fritz.com>
2024-04-05 18:03:31 -0500

    Fix odd player rendering.

--- internal/gameplay/player_render_system.go
+++ internal/gameplay/player_render_system.go
@@ -14,13 +14,13 @@ type playerRenderSystem struct {
 	*engine.Context
 	playerCollection        *entity.Collection
 	physicsComponentManager *component.TypedManager[*physics.PhysicsComponent, physics.PhysicsComponentType]
-	texture                 rendering.Texture
+	emptyTexture            rendering.Texture
 	headAtlas               rendering.Texture
 	runAtlases              []rendering.Texture
 }
 
 func (s *playerRenderSystem) Init() {
-	s.texture = s.TextureLoader.Load("base").Region(7*32, 1*32, 32, 32)
+	s.emptyTexture = s.TextureLoader.Load("base").Region(7*32, 1*32, 32, 32)
 	s.headAtlas = s.TextureLoader.Load("character/headspack/head_1/head_1_1").Region(0, 0, 64, 64)
 	s.runAtlases = []rendering.Texture{
 		s.TextureLoader.Load("character/scientist_1/run_1/sci_run_1_1").Region(0, 0, 64, 64),
@@ -59,9 +59,12 @@ func (s *playerRenderSystem) Process(elapsedMs int64) {
 		x1, y1, x2, y2 := component.Body.CoverBound()
 		w := x2 - x1
 		h := y2 - y1
+		s.SpriteBatch.Draw(s.emptyTexture, x1, y1, w, h, rendering.WithColor(rendering.Color{1, 0, 1, .35}))
 
-		s.SpriteBatch.Draw(s.texture, x1, y1, w, h)
-
+		x1, y1, x2, y2 = component.Body.NonorientedBound()
+		w = x2 - x1
+		h = y2 - y1
+		s.SpriteBatch.Draw(s.emptyTexture, x1, y1, w, h, rendering.WithColor(rendering.Color{1, 1, 0, .35}), rendering.WithRotation(component.Body.Orient), rendering.WithOrigin(w/2, h/2))
 		d := -float32(stdmath.Pi / 2)
 		s.SpriteBatch.Draw(s.runAtlases[int(animationIndex)%len(s.runAtlases)], x1, y1, w, h, rendering.WithRotation(component.Body.Orient+d), rendering.WithOrigin(w/2, h/2))
 		s.SpriteBatch.Draw(s.headAtlas, x1, y1, w, h, rendering.WithRotation(component.Body.Orient-d), rendering.WithOrigin(w/2, h/2))
