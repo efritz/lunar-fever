3dd3af37297a36ed84e31434c18d12fcb8681305
Eric Fritz <eric@eric-fritz.com>
2024-12-31 14:30:15 -0600

    Add point-to-click movement for npc.

--- internal/gameplay/npc_movement_system.go
+++ internal/gameplay/npc_movement_system.go
@@ -7,6 +7,7 @@ import (
 
 type npcMovementSystem struct {
 	*GameContext
+	target *math.Vector
 }
 
 func NewNpcMovementSystem(ctx *GameContext) system.System {
@@ -17,20 +18,33 @@ func (s *npcMovementSystem) Init() {}
 func (s *npcMovementSystem) Exit() {}
 
 func (s *npcMovementSystem) Process(elapsedMs int64) {
+	if s.Mouse.LeftButtonNewlyDown() {
+		mx := s.Camera.Unprojectx(float32(s.Mouse.X()))
+		my := s.Camera.UnprojectY(float32(s.Mouse.Y()))
+		s.target = &math.Vector{mx, my}
+	}
+
 	for _, entity := range s.NpcCollection.Entities() {
 		physicsComponent, ok := s.PhysicsComponentManager.GetComponent(entity)
 		if !ok {
 			continue
 		}
 
-		xDir := float32(1)
-		yDir := float32(1)
 		mod := float32(1000)
 		speed := float32(.35)
 		transitionSpeed := float32(4)
 
-		physicsComponent.Body.LinearVelocity = physicsComponent.Body.LinearVelocity.
-			Muls(1 - (float32(elapsedMs) / mod * transitionSpeed)).
-			Add(math.Vector{xDir, yDir}.Muls(speed * float32(elapsedMs) / mod * transitionSpeed))
+		if s.target != nil {
+			physicsComponent.Body.LinearVelocity =
+				physicsComponent.Body.LinearVelocity.Muls(1 - (float32(elapsedMs) / mod * transitionSpeed)).Add(
+					s.target.Sub(physicsComponent.Body.Position).Normalize().Muls(speed * float32(elapsedMs) / mod * transitionSpeed),
+				)
+
+			if s.target.Sub(physicsComponent.Body.Position).Len() < 2 {
+				s.target = nil
+			}
+		} else {
+			physicsComponent.Body.LinearVelocity = math.Vector{0, 0}
+		}
 	}
 }
