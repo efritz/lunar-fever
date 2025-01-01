8767236ca7f4784af4df51d78f8c7837dbe95a42
Eric Fritz <eric@eric-fritz.com>
2024-12-31 22:45:23 -0600

    Have NPC face direction of movement.

--- internal/gameplay/npc_movement_system.go
+++ internal/gameplay/npc_movement_system.go
@@ -1,6 +1,8 @@
 package gameplay
 
 import (
+	stdmath "math"
+
 	"github.com/efritz/lunar-fever/internal/common/math"
 	"github.com/efritz/lunar-fever/internal/engine/ecs/system"
 )
@@ -18,9 +20,10 @@ func (s *npcMovementSystem) Init() {}
 func (s *npcMovementSystem) Exit() {}
 
 func (s *npcMovementSystem) Process(elapsedMs int64) {
+	mx := s.Camera.Unprojectx(float32(s.Mouse.X()))
+	my := s.Camera.UnprojectY(float32(s.Mouse.Y()))
+
 	if s.Mouse.LeftButtonNewlyDown() {
-		mx := s.Camera.Unprojectx(float32(s.Mouse.X()))
-		my := s.Camera.UnprojectY(float32(s.Mouse.Y()))
 		s.target = &math.Vector{mx, my}
 	}
 
@@ -35,6 +38,16 @@ func (s *npcMovementSystem) Process(elapsedMs int64) {
 		transitionSpeed := float32(4)
 
 		if s.target != nil {
+			angle := math.Atan232(s.target.Y-physicsComponent.Body.Position.Y, s.target.X-physicsComponent.Body.Position.X)
+			if angle < 0 {
+				angle = (2 * stdmath.Pi) - (-angle)
+			}
+			angle -= float32(stdmath.Pi / 2)
+
+			if physicsComponent.Body.Orient != angle {
+				physicsComponent.Body.SetOrient(angle)
+			}
+
 			physicsComponent.Body.LinearVelocity =
 				physicsComponent.Body.LinearVelocity.Muls(1 - (float32(elapsedMs) / mod * transitionSpeed)).Add(
 					s.target.Sub(physicsComponent.Body.Position).Normalize().Muls(speed * float32(elapsedMs) / mod * transitionSpeed),
